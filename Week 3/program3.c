927820b85e0c4839cde22e10c8cc6bbc1ce3f7d4
Yash Chandra <68777570+yash-chandra-23@users.noreply.github.com>
2021-08-11 23:17:55 +0530

    duplicate array

---
+++ Week 3/program3.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int test,arr[20],n,flag=0;
+    printf("Enter the test you want to \n");
+    scanf("%d",&test);
+    for(int k=0;k<test;k++)
+    {
+        printf("Enter the size of the array \n");
+        scanf("%d",&n);
+        printf("Enter the elements of the array \n");
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&arr[i]);
+            
+        }
+        
+        
+       
+        for(int i= 0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                   flag=1;
+                }
+                
+            }
+        }
+        printf("output \n");
+        if(flag== 1)
+        {
+            printf("yes\n");
+            
+        }
+        else
+        {
+            printf("no \n");
+        }
+    }
+    return 0;
+}
