653709329db5f33d68d15b0bb42421c3f6ae9f42
Yash Chandra <68777570+yash-chandra-23@users.noreply.github.com>
2021-08-16 01:54:33 +0530

    Update Merge sort.c

--- Week-4/Merge sort.c
+++ Week-4/Merge sort.c
@@ -1,1 +1,97 @@
+#include <stdio.h>
+int getInvCount(int arr[], int n)
+{
+    int inv_count = 0;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] > arr[j])
+                inv_count++;
+ 
+    return inv_count;
+}
+void merge(int arr[], int p, int q, int r) {
 
+  // Create L ← A[p..q] and M ← A[q+1..r]
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+
+  
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+
+  
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+
+  
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+
+ 
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    
+    merge(arr, l, m, r);
+  }
+}
+
+
+void printArray(int arr[], int n) {
+  for (int i = 0; i < n; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
+}
+
+
+int main() {
+  int arr[20],n;
+  printf("Enter the size of the array \n");
+  scanf("%d",&n);
+  printf("Enter the elements of the array \n");
+  for(int i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+ 
+
+  mergeSort(arr, 0,n- 1);
+
+  printf("Sorted array: \n");
+  printArray(arr,n);
+  
+  printf(" Number of inversions are %d \n",
+           getInvCount(arr, n));
+}
